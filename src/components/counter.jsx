136b89ee3823a7b4ca0ec437c39c91cd80b345bf
Alaamadhoun <alaafm@live.com>
2018-12-29 19:30:05 +0800

    UniqueItemsMapping

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 1,
-    imageUrl: "https://picsum.photos/200"
+    tags: ["tag1", "tag2", "tag3"]
   };
 
   render() {
@@ -12,6 +12,12 @@ class Counter extends Component {
       <div>
         <span className={this.getBadgeClasses()}> {this.formatCount()} </span>
         <button className="btn btn-secondary btn-sm">Increment</button>
+
+        <ul>
+          {this.state.tags.map(tag => (
+            <li key={tag}> {tag} </li>
+          ))}
+        </ul>
       </div>
     );
   }
