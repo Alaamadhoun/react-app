d92db648a32d45573339e3ee43527944d7b697c6
Alaamadhoun <alaafm@live.com>
2018-12-29 19:09:13 +0800

    CartCss

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,25 +1,30 @@
 import React, { Component } from "react";
 class Counter extends Component {
   state = {
-    count: 0,
+    count: 1,
     imageUrl: "https://picsum.photos/200"
   };
 
   render() {
+    let classes = this.getBadgeClasses();
+
     return (
       <div>
-        <span style={{ fontSize: 30 }} className="badge badge-primary m-2 ">
-          {" "}
-          {this.formatCount()}
-        </span>
+        <span className={this.getBadgeClasses()}> {this.formatCount()} </span>
         <button className="btn btn-secondary btn-sm">Increment</button>
       </div>
     );
   }
 
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
   formatCount() {
     const { count } = this.state;
-    return count === 0 ? "Zero" : count;
+    return count === 0 ? "0" : count;
   }
 }
 
