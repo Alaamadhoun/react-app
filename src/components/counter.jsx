f22927a32f390da91f74470ac29389432ba5bc6b
Alaamadhoun <alaafm@live.com>
2018-12-30 02:02:39 +0800

    Display Values from Array

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,21 +1,20 @@
 import React, { Component } from "react";
+
 class Counter extends Component {
   state = {
-    count: 0,
-    tags: ["tag1", "tag2", "tag3"]
+    value: this.props.value
   };
 
   handleIncrement = () => {
-    this.setState({ count: this.state.count + 1 });
+    this.setState({ value: this.state.value + 1 });
   };
-  render() {
-    let classes = this.getBadgeClasses();
 
+  render() {
+    console.log(this.props);
     return (
       <div>
         <span className={this.getBadgeClasses()}>
-          {" "}
-          <h2> {this.formatCount()} </h2>{" "}
+          <h2>{this.formateCount()}</h2>
         </span>
         <button
           onClick={this.handleIncrement}
@@ -23,25 +22,19 @@ class Counter extends Component {
         >
           Increment
         </button>
-
-        <ul>
-          {this.state.tags.map(tag => (
-            <li key={tag}> {tag} </li>
-          ))}
-        </ul>
       </div>
     );
   }
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.state.value === 0 ? "warning" : "primary";
     return classes;
   }
 
-  formatCount() {
-    const { count } = this.state;
-    return count === 0 ? <h2>Zero</h2> : count;
+  formateCount() {
+    const { value } = this.state;
+    return value === 0 ? <h2>Zero</h2> : value;
   }
 }
 
